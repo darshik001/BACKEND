370757c73a60df462f7749124a0f377eb7f22b5e
darshik001 <darshikshekhada07@gmail.com>
2026-01-01 15:47:08 +0530

    crude opration

--- database/app.js
+++ database/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dbConnection = require('./config/dbConaction')
-const Studentsmodel = require('./model/student.model')
+const Studentsmodel = require('./model/student.model');
+const studentModel = require('./model/student.model');
 const port  = 8000;
 const app = express()
 app.set('view engine','ejs')
@@ -9,11 +10,14 @@ app.use(express.urlencoded())
 
 //connection
 dbConnection()
-app.get('/',(req,res)=>{
-    // res.end('Wellcome to Server')
-    return res.render("index")
+app.get('/',async(req,res)=>{
+    
+    let students  = await studentModel.find()
+    return res.render("index",{students})
 })
-
+app.get('/addstudent',(req,res)=>{
+    return res.render('addstudent')
+})
 app.post('/add-student',async(req,res)=>{
     // console.log(req.body)
     try {
@@ -23,6 +27,24 @@ app.post('/add-student',async(req,res)=>{
         console.log(error.errmsg)
     }
 res.redirect('/')
+})
+app.get('/deletestudent/:id',async(req,res)=>{
+    let id  = req.params.id
+    await Studentsmodel.findByIdAndDelete(id)
+    return res.redirect('/')
+
+})
+app.get('/editstudent/:id',async(req,res)=>{
+    let id = req.params.id
+    let student  =await Studentsmodel.findById(id)
+    console.log(student)
+    return res.render('editstudent',{student})
+})
+
+app.post('/update-student/:id',async(req,res)=>{
+    let id  = req.params.id
+   await studentModel.findByIdAndUpdate(id,req.body,{new:true})
+   return res.redirect('/')
 })
 app.listen(port,()=>{
     console.log(`server start at http://localhost:${port}`)
--- database/model/student.model.js
+++ database/model/student.model.js
@@ -1,15 +1,24 @@
 const mongoose = require('mongoose')
 
 const Studentschema = mongoose.Schema({
-    name:{
-        type:String,
-        minLangth:5
-    },
-    email:{
-        type:String,
-                unique:true
-
-    }
+      name: {
+        type:String
+      },
+        email:{
+            type:String
+        },
+        mobile: {
+            type:String
+        },
+        gender: {
+            type:String
+        },
+        dob: {
+            type:String
+        },
+        course: {
+            type:String
+        }
 })
 
 module.exports = mongoose.model ('Students',Studentschema)
