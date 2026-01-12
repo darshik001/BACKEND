1fc5c6347f8af4204de98f5414a5ffcfa0304ec9
darshik001 <darshikshekhada07@gmail.com>
2026-01-12 10:43:56 +0530

    edit and delete

--- admin-penal/controller/admin.controller.js
+++ admin-penal/controller/admin.controller.js
@@ -1,5 +1,7 @@
 const adminModel = require('../model/admin.model')
 const bcrypt = require('bcrypt')
+const path = require('path')
+const fs = require('fs')
 exports.addAdminepage =async (req,res)=>{
     res.render('admin/addAdmin')
 }
@@ -29,9 +31,75 @@ exports.addAdmin = async(req,res)=>{
 
 exports.viewAdmin = async(req,res)=>{
     try {
-        let admins = await  adminModel.find()
+        let search = req.query.params? req.query.params:"";
+        console.log(search)
+        let admins = await  adminModel.find({
+            $or:[
+                {
+                    "fistname":{$regex:search,$options:"i"}
+                },
+                {
+                    "lastname":{$regex:search,$options:"i"}
+                }
+            ]
+        })
         res.render('admin/viewadmin',{admins})
         
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+exports.editAdmin = async(req,res)=>{
+    let id  = req.params.id;
+    try {
+        let admin = await adminModel.findById(id)
+     
+        res.render('admin/editAdmin',{admin})
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+exports.updateAdmin = async(req,res)=>{
+    let id  = req.params.id;
+    
+
+try {
+    let admin = await adminModel.findById(id)
+        let imagepath = admin.profileImage;
+        if(req.file){
+             if(admin.profileImage !== ""){
+                let imageurl = path.join(__dirname,"..",imagepath)
+                await fs.unlinkSync(imageurl)
+             }
+             imagepath = `/uploads/${req.file.filename}`
+        }
+     await adminModel.findByIdAndUpdate(id,{
+        ...req.body,
+        profileImage:imagepath
+     },{new:true})
+        res.redirect('/admin/view-admin')
+    } catch (error) {
+        console.log(error)
+        res.redirect('/admin/view-admin')
+    }
+}
+
+
+exports.deleteAdmin = async(req,res)=>{
+    let id  = req.params.id
+    try {
+        let admin = await adminModel.findById(id)
+        if(admin.profileImage !==""){
+            let imageurl = path.join(__dirname,"..",admin.profileImage)
+            await fs.unlinkSync(imageurl)
+        }
+
+        await adminModel.findByIdAndDelete(id)
+        res.redirect('/admin/view-admin')
+
     } catch (error) {
         console.log(error)
     }
